9d3c428b5a8487f8a9cd19dad831662e52275fa2
John Rojcewicz <rojcewiczj@gmail.com>
2020-01-15 17:52:07 -0600

    working on using classes

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,7 @@
 from room import Room
+from player import Player
+
+
 
 # Declare all the rooms
 
@@ -49,3 +52,58 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# print(type(room))
+
+outside=Room( "Outside Cave Entrance","North of you, the cave mount beckons",['stick', 'rock', 'torch'])
+foyer=Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.",['candle stick','broken mirror'])
+overlook=Room("Grand Overlook","A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", ['old rope', 'empty bucket'] )
+narrow=Room("Narrow Passage","The narrow passage bends here from west to north. The smell of gold permeates the air.", ['only darkness'] )
+treasure=Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.",['dust','ash','note'] )
+
+# intitial player condition
+playerOne = Player(outside.name, ['matches'])
+
+# start of game code
+
+def cls():
+    print("\n" * 100)
+
+
+def OutsideActions():
+   
+   outside_room_options = ["n", "f"]
+   user_choice = " "
+   while user_choice not in outside_room_options:
+        cls()
+        print(F'''you suddenly awake from restles sleep and find yourself at the {outside.name}, {outside.description}...  what do you do?
+       n) enter the cave
+       f) look around for items...''') 
+        user_choice = input("make your choice: ")
+        if user_choice == outside_room_options[0]:
+            foyerActions()
+        elif user_choice == outside_room_options[1]:
+            itemsAction()
+
+def foyerActions():
+    
+    playerOne.location = foyer.name
+    foyer_room_options = ['n','s','e','f']
+    user_choice = ""
+    while user_choice not in foyer_room_options:
+        cls()
+        print(F'''you slowly enter the{foyer.name}, {foyer.description}... what do you do?
+        n) follow the passage toward the overlook
+        s) go back outside
+        e) follow the narrow passage..is that the smell of gold?
+        f) search the foyer for items''')
+        user_choice = input("make your choice ")
+        if user_choice == foyer_room_options[1]:
+            OutsideActions()
+        elif user_choice == foyer_room_options[0]:
+            OverlookOptions()
+
+def OverlookOptions():
+
+    playerOne.location = overlook.name
+OutsideActions()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, location, items=[]):
+        self.name= 'john'
+        self.location = location
+        self.items = items
+    def add_item(self, item):
+        self.items.append(item)
+    
+
+    
